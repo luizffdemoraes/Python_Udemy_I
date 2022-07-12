18fda420b874e81ed5beb5f5d45846b9e1074dd5
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-07-12 00:07:55 -0300

    Sessão 2 - String (texto) e aspas em Python

---
+++ cursopythonyt/aula3/aula3.py
@@ -0,0 +1,15 @@
+"""
+str - string (cadeia de caracter)
+"""
+
+print('alguma coisa')
+print("Aspas duplas")
+print('123456')
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Essa é meu \"texto\" (str).")
+print('Essa é meu \'texto\' (str).')
+print('Essa é meu \n (str).')
+print(r'Essa é meu \n (str).')
+print('Essa é meu "aspas aqui" (str).')
+

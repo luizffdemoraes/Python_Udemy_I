4fd30e357eb82f6037d10b40d77ece4098bba23e
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-07-11 22:45:00 -0300

    Sessão 2 - Comentários de código em Python

---
+++ cursopythonyt/aula1/aula1.py
@@ -0,0 +1,14 @@
+ # Isso é um comentario
+"""
+pode ser usado aspas 3x' ou 3x"
+ Usado para documentação
+ print("teste)
+"""
+print("Meu nome é Luiz")
+
+print("Linha 1")  # outro comentário
+print("Linha 2")
+print("Linha 3")
+print("Linha 4")
+print("Linha 5")
+print("Linha 6")

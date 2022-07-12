84fa8c4aa2614fbd77c6725db15ce7c9b7cc3f37
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-07-11 23:07:52 -0300

    Sessão 2 - O comando print

---
+++ cursopythonyt/aula2/aula2.py
@@ -0,0 +1,11 @@
+# print(123456)
+# print("Luiz", "Felipe", "Outra coisa")
+# print('Luiz', 'Felipe', sep='-', end='#####')
+# print('João', 'e', 'Maria', sep='-', end='')
+
+"""
+824.176.070-18
+"""
+
+print("824","176","070", sep=".", end="-")
+print("18")

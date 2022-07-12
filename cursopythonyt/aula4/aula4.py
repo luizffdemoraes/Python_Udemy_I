7ce17e5aa4dca20c0bd18a7352dfb5b39d39beb9
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-07-12 00:56:12 -0300

    Sessão 2 - Tipos de dados 'primitivos'

---
+++ cursopythonyt/aula4/aula4.py
@@ -0,0 +1,32 @@
+"""
+Tipos de dados
+str - string 'Assim' "Assim"
+int - inteiro - 12345 0 -10 -20 -50 -60 -1500 1500
+float - rela/ponto flutuante - 10.50 1.5 -10.10 -50.93 0.0
+bool - booleano/lógico - True/False 10 == 10
+"""
+
+# print('Luiz', type('Luiz'))
+# print(10, type(10))
+# print(25.23, type(25.23))
+# print('l' == 'L', type('l' == 'L'))
+
+# Type casting converter um tipo para outro
+# print('luiz', type('luiz'), bool('luiz'))
+# print('10', type('10'), type(int('10')))
+
+# String: nome
+print('Nome')
+print('Luiz', type('Luiz'))
+
+# Integer: idade
+print('Idade')
+print(28, type(28))
+
+# Float: altura
+print('Altura')
+print(1.67, type(1.67))
+
+# Boolean: é maior de 18 anos
+print('É maior de 18 anos?')
+print(28 > 18, type(28 > 18))

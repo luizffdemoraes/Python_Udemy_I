122f836e514eb2add8058fa8ed808e7f3870d2bd
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-07-13 23:32:16 -0300

    Sessão 2 - Operadores Aritiméticos

---
+++ cursopythonyt/aula5/aula5.py
@@ -0,0 +1,16 @@
+"""
++, -, *, /. //. **, %, ()
+"""
+
+print("Multiplicação * ", 10 * 10)
+print('Adição + ', 10 + 10)
+print('Subtração - ', 10 - 5)
+print('Divisão / ', 10 / 2)
+print('Divisão Inteira // ', 10 / 2)  # OBS.: A divisão inteira arredonta para um número inteiro.
+print('Potenciação ** ', 2 ** 10)
+print('Resto % ', 10 % 3)
+print("() alteram a precedência dos operadores.")
+print(5+2*10)
+print((5+2)*10)
+
+print('Luiz' + ' ' + 'Felipe e ele tem ' + str(28) + ' anos')
